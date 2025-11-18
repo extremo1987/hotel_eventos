39a4873889473d98b962d984d33b01b3c45ce17d
extremo1987 <59928260+extremo1987@users.noreply.github.com>
2025-11-17 19:22:31 -0600

    Add files via upload

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -1,9 +1,2 @@
 <?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Package extends Model {
-  use HasFactory;
-  protected $fillable = ['name','description','price','is_active'];
-}
+// model placeholder
